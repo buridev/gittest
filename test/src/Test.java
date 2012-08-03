5e62f2bd26d55fdc195e7b1f3d8be3c4b5afe57f
buridev <buridev@gmail.com>
2012-08-04 00:10:45 +0900

    Init

---
+++ test/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nice know that");
+		System.out.println("Is this going to remote github?");
+	}
+
+}
